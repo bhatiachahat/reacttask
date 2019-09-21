27e38b63081e142cebb703dbdd1a3413b6a16b88
bhatia100 <chahatbhatia2014@gmail.com>
2019-09-22 00:30:54 +0530

    Updates

---
+++ precache-manifest.055031bc9090e1101ac7c669e8ddb5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d14b2563b88ced4634f8b90e8e535e",
+    "url": "/reacttask/index.html"
+  },
+  {
+    "revision": "8a7f9100b32e724c51af",
+    "url": "/reacttask/static/css/main.55a69d19.chunk.css"
+  },
+  {
+    "revision": "6bd142d7fd5b19f399ae",
+    "url": "/reacttask/static/js/2.9ade907c.chunk.js"
+  },
+  {
+    "revision": "8a7f9100b32e724c51af",
+    "url": "/reacttask/static/js/main.e8220c7c.chunk.js"
+  },
+  {
+    "revision": "31302a65c53c1f39f211",
+    "url": "/reacttask/static/js/runtime~main.fe36ad7c.js"
+  },
+  {
+    "revision": "d94c375b634d3144520694aacd8ed4cf",
+    "url": "/reacttask/static/media/cover4.d94c375b.jpg"
+  }
+]);
